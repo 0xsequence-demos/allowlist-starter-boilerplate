a8d6c5cb23bd354c63550047a6df57e7a48332ba
Ahmet Buğra Yiğiter <yigiterahmetbugra@gmail.com>
2025-01-07 15:24:38 +0300

    Add env variable for wallet type

--- src/config.ts
+++ src/config.ts
@@ -7,9 +7,24 @@ const chainId = Number(import.meta.env.VITE_CHAIN_ID);
 const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 const appleClientId = import.meta.env.VITE_APPLE_CLIENT_ID;
 const appleRedirectURI = window.location.origin + window.location.pathname;
+const walletConnectProjectId = import.meta.env.VITE_WALLETCONNECT_PUBLIC_ID;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const config: any = createConfig("waas", {
+export const universalConfig: any = createConfig("universal", {
+  projectAccessKey: projectAccessKey,
+  chainIds: [chainId],
+  defaultChainId: chainId,
+  appName: "Kit Starter",
+  google: true,
+  apple: true,
+  walletConnect: {
+    projectId: walletConnectProjectId,
+  },
+  metaMask: true,
+});
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const embeddedConfig: any = createConfig("waas", {
   projectAccessKey: projectAccessKey,
   chainIds: [chainId],
   defaultChainId: chainId,
